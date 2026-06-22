2ddf5df1891ca394539b4861da8b5302e2a6958a
polina <polina-shestakova2003@mail.ru>
2026-06-22 17:56:00 +0500

    README and Procfile

--- resources/views/emails/owner.blade.php
+++ resources/views/emails/owner.blade.php
@@ -1,14 +1,15 @@
 <h2>Новая заявка с сайта</h2>
 
-<p><strong>Имя:</strong> {{ $request['name'] }}</p>
-<p><strong>Email:</strong> {{ $request['email'] }}</p>
-<p><strong>Комментарий:</strong> {{ $request['comment'] }}</p>
+<p><strong>Имя:</strong> {{ $data['request']['name'] }}</p>
+<p><strong>Email:</strong> {{ $data['request']['email'] }}</p>
+<p><strong>Телефон:</strong> {{ $data['request']['phone'] }}</p>
+<p><strong>Сообщение:</strong> {{ $data['request']['comment'] }}</p>
 
 <hr>
 
 <h3>AI анализ</h3>
-<p><strong>Тональность:</strong> {{ $request['sentiment'] }}</p>
-<p><strong>Ответ AI:</strong> {{ $request['ai_reply'] }}</p>
-<p><strong>AI использован:</strong> {{ $request['ai_used'] ? 'Да' : 'Нет' }}</p>
+<p><strong>Тональность:</strong> {{ $data['request']['sentiment'] }}</p>
+<p><strong>Ответ AI:</strong> {{ $data['request']['ai_reply'] }}</p>
+<p><strong>AI использован:</strong> {{ $data['request']['ai_used'] ? 'Да' : 'Нет' }}</p>
 
-<p><strong>Дата:</strong> {{ $request['created_at'] }}</p>
+<p><strong>Дата:</strong> {{ $data['request']['created_at'] }}</p>
